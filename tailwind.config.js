67ef9aab8d6c8572f640a64e766357cd3d2d5351
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-10-04 11:53:32 +0530

    added assets, updated background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,37 @@
 module.exports = {
   content: [ "./src/**/*.html", "./src/**/*.js" ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-desktop-bg': "url('/src/assets/images/pattern-background-desktop.svg')",
+        'custom-mobile-bg': "url('/src/assets/images/pattern-background-mobile.svg')",
+      },
+      colors: {
+        primary: {
+          pale: 'hsl(225, 100%, 94%)',
+          bright: 'hsl(245, 75%, 52%)',
+        },
+        neutral: {
+          'very-pale': 'hsl(225, 100%, 98%)',
+          'desaturated': 'hsl(224, 23%, 55%)',
+          'dark': 'hsl(223, 47%, 23%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
 
+/*
+Add colors
+### Primary
+
+- Pale blue: hsl(225, 100%, 94%)
+- Bright blue: hsl(245, 75%, 52%)
+
+### Neutral
+
+- Very pale blue: hsl(225, 100%, 98%)
+- Desaturated blue: hsl(224, 23%, 55%)
+- Dark blue: hsl(223, 47%, 23%)
+*/
